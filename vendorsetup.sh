13313beb45b592fda5bc2e067d46b780c4baf26f
Federico Saccà <feduss96@gmail.com>
2021-09-28 22:16:48 +0200

    First twrp build with permissive selinux

---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_T8116-user
+add_lunch_combo omni_T8116-userdebug
+add_lunch_combo omni_T8116-eng
